4f6cd70041367530c6cfab9df768402354172ed5
carlosj3mit <carlos@3mit.dev>
2022-09-25 14:30:21 -0400

    feat: creating polls views

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
 	path('<int:question_id>/', views.detail, name='index'),
 	path('<int:question_id>/results/', views.results, name='index'),
 	path('<int:question_id>/vote/', views.vote, name='index'),
-
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from polls.models import Question
 
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>estas en la pagina principal de premios platzi app</h1>")
-
+	questions = Question.objects.all()
+	return render(request, 'polls/index.html', {
+		'lastest_question_list': questions
+	})
 
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(id=question_id)
-		return HttpResponse('Estas viendo la pregunta numero {}\n\n{}'.format(question_id, question.question_text))
-	except:
-		return HttpResponse('ha ocurrido un error')
+	question = get_object_or_404(Question, id=question_id)
+	return render(request, 'polls/detail.html', {
+		'question': question
+	})
+	# try:
+	# 	question = Question.objects.get(id=question_id)
+	# 	return HttpResponse('Estas viendo la pregunta numero {}\n\n{}'.format(question_id, question.question_text))
+	# except:
+	# 	return HttpResponse('ha ocurrido un error')
 
 def results(request, question_id):
 	return HttpResponse('Estas viendo los resultados de la pregunta numero {}'.format(question_id))
-
 
 def vote(request, question_id):
 	return HttpResponse('Estas votando a la pregunta {}'.format(question_id))
