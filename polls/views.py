8ed94e502504de2765f07dd333a5cf80b69a46b8
carlosj3mit <carlos@3mit.dev>
2022-09-25 13:18:51 -0400

    feat: adding more views to the project

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,23 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
+
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>hello  World</h1><h3>testing</h3>")
+	return HttpResponse("<h1>estas en la pagina principal de premios platzi app</h1>")
+
+
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(id=question_id)
+		return HttpResponse('Estas viendo la pregunta numero {}\n\n{}'.format(question_id, question.question_text))
+	except:
+		return HttpResponse('ha ocurrido un error')
+
+def results(request, question_id):
+	return HttpResponse('Estas viendo los resultados de la pregunta numero {}'.format(question_id))
+
+
+def vote(request, question_id):
+	return HttpResponse('Estas votando a la pregunta {}'.format(question_id))
