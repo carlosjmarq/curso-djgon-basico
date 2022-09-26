3ab5e15ac0d2c72da22082f440c8871df82ccf62
carlosj3mit <carlos@3mit.dev>
2022-09-26 07:51:49 -0400

    refact: changing function based views for generic views

--- polls/views.py
+++ polls/views.py
@@ -3,26 +3,44 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from polls.models import Question, Choice
+from django.views import generic
 
 
 # Create your views here.
-def index(request):
-	questions = Question.objects.all()
-	return render(request, 'polls/index.html', {
-		'lastest_question_list': questions
-	})
+# def index(request):
+# 	questions = Question.objects.all()
+# 	return render(request, 'polls/index.html', {
+# 		'lastest_question_list': questions
+# 	})
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, id=question_id)
-	return render(request, 'polls/detail.html', {
-		'question': question
-	})
+# def detail(request, question_id):
+# 	question = get_object_or_404(Question, id=question_id)
+# 	return render(request, 'polls/detail.html', {
+# 		'question': question
+# 	})
 
-def results(request, question_id):
-	question = get_object_or_404(Question, id=question_id)
-	return render(request, 'polls/results.html', {
-		"question": question
-	})
+# def results(request, question_id):
+# 	question = get_object_or_404(Question, id=question_id)
+# 	return render(request, 'polls/results.html', {
+# 		"question": question
+# 	})
+
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'lastest_question_list'
+
+	def get_queryset(self):
+		# * return the five latest elements
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
